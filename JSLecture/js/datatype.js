a92007f15c8c25503e58dc07e6c1b6e93f8ce52e
Phyo-Min-Hlaing <phyo1401@gmail.com>
2026-01-14 22:20:59 +0630

    JSLecture

---
+++ JSLecture/js/datatype.js
@@ -0,0 +1,28 @@
+// String data type
+
+let name = "Phyo Min Hlaing";
+console.log(name);
+console.log(typeof(name))
+
+// Numeric data typeof (""'' ``) အာ့တွပါရင် string ဖြစ်သွားပီး ပေါင်းနုတ်မြှောက်စားမရတော့ပါ
+
+let age = 29; 
+console.log(age);
+console.log(typeof(age));
+
+// bollen data type
+
+let single = true;
+console.log(single);
+console.log(typeof(single));
+
+// undified
+
+let myBaby;
+console.log(myBaby);
+
+// null
+
+let son = null;
+console.log(son);
+console.log(typeof(son));
